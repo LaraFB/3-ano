0b02a2d1584a278a68fb31ff2836fe220d2970fd
Francisco Costa <a2021146317@isec.pt>
2023-09-22 17:01:12 +0100

    BeginProject

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.gps_g11 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.gps_g11 to javafx.fxml;
+    exports com.example.gps_g11;
+}
