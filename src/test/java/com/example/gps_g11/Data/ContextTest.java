bfee920bcd78739db089ce368664f991c38d486e
Lara Bizarro <a2021130066@isec.pt>
2023-11-13 11:27:23 +0000

    test

---
+++ src/test/java/com/example/gps_g11/Data/ContextTest.java
@@ -0,0 +1,14 @@
+package com.example.gps_g11.Data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContextTest {
+
+    @Test
+    void getValorBolsa() {
+        Context context = Context.getInstance();
+        assertEquals(context.getValorBolsa(),120);
+    }
+}
