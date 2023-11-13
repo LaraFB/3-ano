dc76ed8881b73ff43b31fba252f1b22349aa8332
Lara Bizarro <a2021130066@isec.pt>
2023-11-13 19:24:08 +0000

    testes context

--- src/test/java/com/example/gps_g11/Data/ContextTest.java
+++ src/test/java/com/example/gps_g11/Data/ContextTest.java
@@ -1,14 +1,47 @@
 package com.example.gps_g11.Data;
 
+import com.example.gps_g11.Data.categoryManagment.CategoryHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContextTest {
-
+    Context context = Context.getInstance();
+    @Test
+    void getBudgetRestante (){
+        assertEquals(context.getBudgetRestante(),200);
+    }
+    @Test
+    void getBudgetGasto(){
+        assertEquals(context.getBudgetGasto(),49);
+    }
+    @Test
+    void isEmpty(){
+        //por fazer
+    }
     @Test
     void getValorBolsa() {
-        Context context = Context.getInstance();
         assertEquals(context.getValorBolsa(),110);
+    }
+    @Test
+    void getValorGastoBolsa(){
+        assertEquals(context.getValorGastoBolsa(),10);
+    }
+
+    @Test
+    void getTotalExpenses(){
+        //por fazer
+    }
+    @Test
+    void getExpensesByCategory(){
+        //por fazer
+    }
+    @Test
+    void getExpensesByDate(){
+        //por fazer
+    }
+    @Test
+    void ExpensestoString(){
+        //por fazer
     }
 }
---
+++ src/test/java/com/example/gps_g11/Data/categoryManagment/CategoryHandlerTest.java
@@ -0,0 +1,46 @@
+package com.example.gps_g11.Data.categoryManagment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryHandlerTest {
+
+    @Test
+    void addCategory() {
+    }
+    @Test
+    void testAddCategory() {
+    }
+    @Test
+    void testAddCategory1() {
+    }
+    @Test
+    void removeCategory() {
+    }
+    @Test
+    void testRemoveCategory() {
+        //por fazer
+    }
+    @Test
+    void testRemoveCategory1() {
+        //por fazer
+    }
+    @Test
+    void editCategoryName() {
+        //por fazer
+    }
+    @Test
+    void editCategoryDescription() {
+        //por fazer
+    }
+    @Test
+    void testEditCategoryName()
+    {
+    }
+    @Test
+    void testEditCategoryDescription() {
+        //por fazer
+    }
+
+}
